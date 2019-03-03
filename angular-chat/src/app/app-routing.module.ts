68e9ef261ee603d3569e3ac3fb69eda29923cf4c
Hanna Tydén <dic15hty@student.lu.se>
2019-03-03 11:14:45 +0100

    Fix suggestion page

--- angular-chat/src/app/app-routing.module.ts
+++ angular-chat/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ResultComponent } from './result/result.component';
 import {HomeComponent} from './home/home.component';
+import {SuggestedComponent} from './suggested/suggested.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'result',
     component: ResultComponent,
+  },
+  {
+    path: 'suggested',
+    component: SuggestedComponent,
   }
 ];
 
