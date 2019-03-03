b6b44016bbe63828776569dd4baeb15352cb1e11
Gustav Lilja <lilja.gustav@student.lu.se>
2019-03-03 11:28:50 +0100

    Fix merge conflicts

--- angular-chat/src/app/app.module.ts
+++ angular-chat/src/app/app.module.ts
@@ -1,12 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FoodFetcherComponent } from './food-fetcher/food-fetcher.component';
-=======
 import { MatCheckboxModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,19 +8,14 @@ import { ResultComponent } from './result/result.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SuggestedComponent } from './suggested/suggested.component';
->>>>>>> master
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    FoodFetcherComponent
-=======
     ResultComponent,
     HeaderComponent,
     HomeComponent,
     SuggestedComponent,
->>>>>>> master
   ],
   imports: [
     HttpClientModule,
